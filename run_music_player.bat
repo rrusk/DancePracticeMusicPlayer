626d65f38b2193d45acfbbd0a778156688c7c037
Raymond Rusk <Raymond.Rusk@shaw.ca>
2024-09-17 16:17:41 -0700

    add installation comment to bat script

--- run_music_player.bat
+++ run_music_player.bat
@@ -1,3 +1,10 @@
+:: To Add to Desktop
+:: If you save the .bat file on the Desktop, it's already accessible.
+:: If you want to add it as a shortcut:
+::    Right-click the .bat file and select Send to -> Desktop (create shortcut).
+::    Optionally, you can change the icon by right-clicking the shortcut,
+:     selecting Properties, and then choosing a different icon under the Shortcut tab.
+:: Now, you can run your Kivy application by simply double-clicking the .bat file on your desktop!
 @echo off
 cd /d "C:\Users\vbds_\git\DancePracticeMusicPlayer\kivy_venv\Scripts"
 call activate
