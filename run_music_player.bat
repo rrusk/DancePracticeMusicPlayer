978855110271c2598ee449956b8fa032923bff35
Raymond Rusk <Raymond.Rusk@shaw.ca>
2024-09-17 17:11:29 -0700

    clarify Windows bat path requirements

--- run_music_player.bat
+++ run_music_player.bat
@@ -1,3 +1,9 @@
+:: Set paths for Kivy scripts and music_player.py (adjust as needed).
+set KIVY_PATH=C:\Users\vbds_\git\DancePracticeMusicPlayer\kivy_venv\Scripts
+set MUSIC_PLAYER_PATH=C:\Users\vbds_\git\DancePracticeMusicPlayer
+
+set PATH=%KIVY_PATH%;%MUSIC_PLAYER_PATH%;%PATH%
+
 :: To Add to Desktop
 :: If you save the .bat file on the Desktop, it's already accessible.
 :: If you want to add it as a shortcut:
@@ -6,8 +12,8 @@
 :     selecting Properties, and then choosing a different icon under the Shortcut tab.
 :: Now, you can run your Kivy application by simply double-clicking the .bat file on your desktop!
 @echo off
-cd /d "C:\Users\vbds_\git\DancePracticeMusicPlayer\kivy_venv\Scripts"
+cd /d %KIVY_PATH%
 call activate
-cd /d "C:\Users\vbds_\git\DancePracticeMusicPlayer"
+cd /d %MUSIC_PLAYER_PATH%
 python music_player.py
 pause
