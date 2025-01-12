891dbce22ca1b3fc7ad7d2b491dc42606cb4eb9c
Raymond Rusk <rusk.raymond@gmail.com>
2025-01-12 12:52:17 -0800

    tweaked replay icon

--- icons/svg/svg_png.sh
+++ icons/svg/svg_png.sh
@@ -6,6 +6,6 @@ convert +negate -background "rgb(18,53,70)" -resize 48x48 music-play-button-svgr
 convert +negate -background "rgb(18,53,70)" -resize 48x48 media-player-music-pause-svgrepo-com.svg pause.png
 #From https://www.svgrepo.com/svg/424374/music-player-stop?edit=true
 convert +negate -background "rgb(18,53,70)" -resize 48x48 music-player-stop-svgrepo-com.svg stop.png
-#convert +negate -background "rgb(18,53,70)" -resize 48x48 replay.svg replay.png
-convert +negate -background "rgb(18,53,70)" -resize 48x48 IEC_60417_-_Ref-No_5125A.svg replay.png
+#convert +negate -background "rgb(18,53,70)" -resize 48x48 IEC_60417_-_Ref-No_5125A.svg replay.png
+convert +negate -background "rgb(18,53,70)" -resize 48x48 replay.svg replay.png
 mv play.png pause.png stop.png replay.png ../
