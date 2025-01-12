ce50b48bf8e4b6160a8d53d9d156d5b757b6d61c
Raymond Rusk <rusk.raymond@gmail.com>
2025-01-11 21:17:47 -0800

    tweaked icons

--- icons/svg/svg_png.sh
+++ icons/svg/svg_png.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/sh
-# Note: used gpick to match background of icons to text buttons
-convert +negate -background "rgb(18,53,70)" -resize 48x48 play-filled-alt.svg play.png
-convert +negate -background "rgb(18,53,70)" -resize 48x48 pause-filled.svg pause.png
-convert +negate -background "rgb(18,53,70)" -resize 48x48 stop.svg stop.png
-convert +negate -background "rgb(18,53,70)" -resize 48x48 replay.svg replay.png
+# Note: used gpick to match background color of icons to text buttons
+# From https://www.svgrepo.com/svg/107252/music-play-button?edit=true, Icon size 200px, padding 35%
+convert +negate -background "rgb(18,53,70)" -resize 48x48 music-play-button-svgrepo-com.svg play.png
+# From https://www.svgrepo.com/svg/424370/media-player-music-pause?edit=true, Icon size 200px, padding 35%
+convert +negate -background "rgb(18,53,70)" -resize 48x48 media-player-music-pause-svgrepo-com.svg pause.png
+#From https://www.svgrepo.com/svg/424374/music-player-stop?edit=true
+convert +negate -background "rgb(18,53,70)" -resize 48x48 music-player-stop-svgrepo-com.svg stop.png
+#convert +negate -background "rgb(18,53,70)" -resize 48x48 replay.svg replay.png
+convert +negate -background "rgb(18,53,70)" -resize 48x48 IEC_60417_-_Ref-No_5125A.svg replay.png
+mv play.png pause.png stop.png replay.png ../
